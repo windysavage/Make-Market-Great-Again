0a8bffec1a8a9ca99e1f739c22ee60550e02324f
windysavage <water92001@gmail.com>
2025-07-13 23:09:52 +0800

    add def schedule

--- dagster/dagster_project/repository.py
+++ dagster/dagster_project/repository.py
@@ -1,4 +1,12 @@
-from dagster import Definitions
+from dagster import Definitions, ScheduleDefinition
 from dagster_project.jobs import send_welcome_email_job, watch_trump_post_job
 
-defs = Definitions(jobs=[send_welcome_email_job, watch_trump_post_job])
+hourly_schedule = ScheduleDefinition(
+    job=watch_trump_post_job,
+    cron_schedule='0 * * * *',
+    name='hourly_schedule',
+)
+
+defs = Definitions(
+    jobs=[send_welcome_email_job, watch_trump_post_job], schedules=[hourly_schedule]
+)
